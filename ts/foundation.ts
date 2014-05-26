4930813c4d2894786abb0b8ec2e53767e8eda5e4
Twijgjes <roanhageman@gmail.com>
2014-05-26 22:02:43 +0200

    Some more work on basic objects.

--- ts/foundation.ts
+++ ts/foundation.ts
@@ -1,7 +1,3 @@
-/**
- * Created by roanh_000 on 21/05/2014.
- **/
-
 /// <reference path="Main.ts" />
 
 module CapBat {
@@ -17,11 +13,41 @@ module CapBat {
 
   export class Entity extends GameObject {
 
-    private p: CapBat.Math.Vec2;
+    private p: CapBat.Vec2;
 
-    constructor(gameRef: CapBat.Game, pos: CapBat.Math.Vec2) {
+    constructor(gameRef: CapBat.Game, pos: CapBat.Vec2) {
       super(gameRef);
-      this.p = pos.copy();
+      this.p = CapBat.Vec2.returnClone( pos );
+    }
+
+    public update( speed ) {
+
+    }
+
+    get p(): Vec2 {
+      return CapBat.Vec2.returnClone(this.p);
+    }
+
+    set p( v: Vec2 ) {
+      this.p.set(v);
+    }
+  }
+
+  export class Drawable extends Entity {
+
+    private c: string;
+
+    constructor(gameRef: CapBat.Game, pos: CapBat.Vec2, color: string, parent: CapBat.Entity = null){
+      super(gameRef, pos);
+      this.c = color;
+    }
+
+    public draw( canvas, context ) {
+      context.save();
+      context.translate( this.p.x, this.p.y );
+      context.fillStyle = this.c;
+      context.fillRect(5, 5, 10, 10);
+      context.restore();
     }
   }
 }
--- ts/math.ts
+++ ts/math.ts
@@ -2,91 +2,87 @@
  * Created by roanh_000 on 21/05/2014.
  */
 module CapBat {
-  export class Math {
-    public static getNormalBetweenVecs( from: CapBat.Math.Vec2, to: CapBat.Math.Vec2 ) {
-      var fromV = new CapBat.Math.Vec2().clone(from),
-          toV = new CapBat.Math.Vec2().clone(to);
+  export class MathUtils {
+    public static getNormalBetweenVecs( from: CapBat.Vec2, to: CapBat.Vec2 ) {
+      var fromV = new CapBat.Vec2().set(from),
+          toV = new CapBat.Vec2().set(to);
     }
 
   }
+  export class Vec2 {
 
-  export module Math {
-    export class Vec2 {
+    public x: number;
+    public y: number;
 
-      public x: number;
-      public y: number;
+    constructor(x?: number, y?: number){
+      this.x = x;
+      this.y = y;
+    }
 
-      constructor(x?: number, y?: number){
-        this.x = x;
-        this.y = y;
+    public add( v: CapBat.Vec2 ) {
+      this.x += v.x;
+      this.y += v.y;
+    }
+
+    public sub( v: CapBat.Vec2 ) {
+      this.x += v.x;
+      this.y += v.y;
+    }
+
+    public multiplyScalar( s: number ) {
+      this.y *= s;
+      this.x *= s;
+    }
+
+    public normalize( ) {
+      var mag = 1 / this.magnitude();
+      if ( mag != 0 ) {
+        this.x *= mag;
+        this.y *= mag;
       }
+    }
 
-      public add( v: CapBat.Math.Vec2 ) {
-        this.x += v.x;
-        this.y += v.y;
-      }
+    public magnitude( ) {
+      return Math.sqrt( ( this.x * this.x ) + ( this.y * this.y ) );
+    }
 
-      public sub( v: CapBat.Math.Vec2 ) {
-        this.x += v.x;
-        this.y += v.y;
-      }
+    public distanceTo( v: CapBat.Vec2 ) {
+      var va = new CapBat.Vec2( v.x, v.y ),
+          vb = new CapBat.Vec2( this.x, this.y );
 
-      public multiplyScalar( s: number ) {
-        this.y *= s;
-        this.x *= s;
-      }
+      va.sub( vb );
+      return va.magnitude();
+    }
 
-      public dot( v: CapBat.Math.Vec2 ) {
-        // Whups, nuffin yet
-        console.log( 'HEY! You still have to write this function.' );
-      }
+    public negate( ) {
+      this.x *= -1;
+      this.y *= -1;
+      return this;
+    }
 
-      public normalize( ) {
-        var mag = 1 / this.magnitude();
-        if ( mag != 0 ) {
-          this.x *= mag;
-          this.y *= mag;
-        }
-      }
+    public angleTo( v: CapBat.Vec2 ) {
+      var normal = MathUtils.getNormalBetweenVecs( v, this );
+      return Math.atan2( normal.y, normal.x );
+    }
 
-      public magnitude( ) {
-        return Math.sqrt( ( this.x * this.x ) + ( this.y * this.y ) );
-      }
+    public placeAround( rot: number, v: CapBat.Vec2, radius: number ) {
+      this.x = v.x + Math.cos(rot) * radius;
+      this.y = v.y + Math.sin(rot) * radius;
+    }
 
-      public distanceTo( v: CapBat.Math.Vec2 ) {
-        var va = new CapBat.Math.Vec2( v.x, v.y ),
-            vb = new CapBat.Math.Vec2( this.x, this.y );
+    public set( v: CapBat.Vec2 ) {
+      this.x = v.x;
+      this.y = v.y;
+      return this;
+    }
 
-        va.sub( vb );
-        return va.magnitude();
-      }
+    public copy( v: CapBat.Vec2 ) {
+      this.set( v );
+      return this;
+    }
 
-      public negate( ) {
-        this.x *= -1;
-        this.y *= -1;
-        return this;
-      }
-
-      public angleTo( v: CapBat.Math.Vec2 ) {
-        var normal = CapBat.Math.getNormalBetweenVecs( v, this );
-        return Math.atan2( normal.y, normal.x );
-      }
-
-      public placeAround( rot: number, v: CapBat.Math.Vec2, radius: number ) {
-        this.x = v.x + Math.cos(rot) * radius;
-        this.y = v.y + Math.sin(rot) * radius;
-      }
-
-      public set( v: CapBat.Math.Vec2 ) {
-        this.x = v.x;
-        this.y = v.y;
-        return this;
-      }
-
-      public clone( v: CapBat.Math.Vec2 ) {
-        this.set( v );
-        return this;
-      }
+    public static returnClone( v ) {
+      return new CapBat.Vec2(v.x, v.y);
     }
   }
 }
